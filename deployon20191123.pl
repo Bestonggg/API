c83d06abb02806da6d769e74f0eda414e41f17c0
Oleg Antipov <info.oleg.antipov@gmail.com>
2019-12-05 16:40:11 +0300

    deploy

--- deployon20191123.pl
+++ deployon20191123.pl
@@ -3,5 +3,5 @@ echo "Content-type: text/plain\n";
 echo '';
 cd /var/www/domains/g2-public.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
-git pull ssh://gafurovstudio@bitbucket.org/gafurovstudio/g2-public.git develop > /dev/null
+git pull ssh://gafurovstudio@bitbucket.org/gafurovstudio/g2_public.git develop > /dev/null
 #git@bitbucket.org:gafurovstudio/g2.git
