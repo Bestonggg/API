606f8c8850b51e70e0ed173c86c87a469c82a23c
Олег Антипов <oleg@gafurov.studio>
2020-03-02 13:41:51 +0000

    deploy.pl edited online with Bitbucket

--- deploy.pl
+++ deploy.pl
@@ -3,5 +3,5 @@ echo "Content-type: text/plain\n";
 echo '';
 cd /var/www/domains/site.ru/ || exit > /dev/null
 git reset --hard > /dev/null
-git pull ssh://gafurovstudio@bitbucket.org/gafurovstudio/g2.git develop > /dev/null
-#git@bitbucket.org:gafurovstudio/g2.git
+git pull ssh_ссылка_на_репозитрий develop > /dev/null
+chomod -R 755 /var/www/domains/site.ru/ 
