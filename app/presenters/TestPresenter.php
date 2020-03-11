4734924a7c5ce97998aeff7af4dfe6d018fac670
Bestonggg <Dj_Cosmonavt@mail.ru>
2020-03-11 17:52:35 +0300

    test1.5

--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -11,11 +11,12 @@ class TestPresenter extends _MainPresenter {
 
 	public function getListUsers(){
 
-		if($this->isSecurity()){
-			echo (new Users())->getListUsers();
+	echo "123";
+		//if($this->isSecurity()){
+			//echo (new Users())->getListUsers();
 			
 			
-		} 
+		//} 
 
 	}
 
