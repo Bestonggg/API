b5d259020c28b443fb3d2836a6df2e4993995f32
Bestonggg <Dj_Cosmonavt@mail.ru>
2020-03-11 17:15:28 +0300

    test

--- app/models/Users.php
+++ app/models/Users.php
@@ -9,18 +9,18 @@ class Users extends _MainModel{
 
         var_dump(self::check('id'));
         
-        // if(self::check(self::$params_url['id'])){
-        // 	echo 1;
-        // }else{
-        // 	echo 2;
+         if(self::check(self::$params_url['id'])){
+         	echo 1;
+         }else{
+        	echo 2;
 
-        // }
-        // if( array_key_exists("id", self::$params_url) ){
-        // 	// if( self::$params_url['id'] != ''){
-        // 	var_dump("есть");
-        // }else{
-        // 	var_dump("пусто");
-        // }
+         }
+         if( array_key_exists("id", self::$params_url) ){
+         	if( self::$params_url['id'] != ''){
+         	var_dump("есть");
+        }else{
+        	var_dump("пусто");
+         }
         
         _MainModel::viewJSON($result);  
     }
