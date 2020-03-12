8991629c73d33ae9542346d2381d2e36cea07658
Bestonggg <Dj_Cosmonavt@mail.ru>
2020-03-12 14:51:15 +0300

    1.8

---
+++ app/presenters/Test2Presenter.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Every class derriving from Model has access to $this->db
+ * $this->db is a PDO object
+ * Has a config in /core/config/database.php
+ * branch b1
+ */
+
+class Test2Presenter extends _MainPresenter {
+
+	public function printJSON(){
+
+	echo json_encode ( mixed $value [, int $options = 0 [, int $depth = 512 ]] ) : string;
+	
+		} 
+*/
+	}
+
+	
+
+
+
+?>
+ 
