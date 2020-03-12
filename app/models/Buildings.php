263597942e2056b790103cf5a862b972de0bcbbe
Bestonggg <Dj_Cosmonavt@mail.ru>
2020-03-12 17:31:31 +0300

    1.8

--- app/models/Buildings.php
+++ app/models/Buildings.php
@@ -3,7 +3,7 @@ class Buildings extends _MainModel{
 
 public function getListBuildings(){
 
-$result= _MainModel::table("buildings")->get(array("id", "town","street","number"))->filter(array("street" =>"Lenin" )->send();
+$result= _MainModel::table("buildings")->get(array("id", "town","street","number"))->filter(array("id" =>1 )->send();
 _MainModel::viewJSON($result);
 }
 
