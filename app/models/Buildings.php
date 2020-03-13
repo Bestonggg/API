db7b93a72a036e9eccb50cc035f3040173a466d0
Bestonggg <Dj_Cosmonavt@mail.ru>
2020-03-13 14:31:28 +0300

    1.8

--- app/models/Buildings.php
+++ app/models/Buildings.php
@@ -1,21 +1,25 @@
 <?
 class Buildings extends _MainModel{
 
+public $town=" ";
+public $street=" ";
+public $number="";
 
-
+//Вывод списка зданий
 public function getListBuildings(){
+
 
 $result= _MainModel::table("buildings")->get(array("id", "town","street","number"))->filter(array("street" =>"Lenin"))->pagination(0,6)->send();
 _MainModel::viewJSON($result);
 }
 
-
+//Добавление записи
 public function addNewBuilding(){
 $test=_MainModel::table("buildings")->add(array("town" => 7, "street" => 7,"number"=>7))->send(); 
 _MainModel::viewJSON($test);
 }
 
-
+//Редактирование записи
 public function updateBuilding(){
 _MainModel::table("buildings")->edit(array("town" => "значение1", "street" =>"значение2","number"=>"value3"), array("id" => 1))->send();
 }
