3450f32b9c83aa620faf92b7db3d3e286ba2c23e
Bestonggg <43250876+Bestonggg@users.noreply.github.com>
2020-03-18 16:15:59 +0300

    1.8

--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -2,11 +2,16 @@
  
 class TestPresenter extends _MainPresenter{
 
-public function infoG(){
+public function infoJ(){
 
 $test=_MainModel::viewJSON(["error" => "Данные не найдены"]);
 
 echo $test;
+}
+
+public function info() {
+
+echo "Эщекере";
 }
 
 }
