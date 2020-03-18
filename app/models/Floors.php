457c4122d443b609131fecd84d8f14f3e9543f08
Bestonggg <43250876+Bestonggg@users.noreply.github.com>
2020-03-18 18:49:03 +0300

    1.8

--- app/models/Floors.php
+++ app/models/Floors.php
@@ -30,6 +30,7 @@ if(isset($_GET['building_id'])){
 
 //Вывод списка этажей
 public function getListFloors(){
+
 $result= _MainModel::table("floors")->get(array("id", "plan","building_id"))->pagination(0,6)->send();
 if($result!=null)
 _MainModel::viewJSON($result);
@@ -38,19 +39,37 @@ else _MainModel::viewJSON(["error" => "Данные не найдены"]);
 }
 //Добавление этажа
 public function addNewFloor(){
+if(isset($_GET['id'])&&isset($_GET['plan'])&&isset($_GET['building_id'])){
 $result=_MainModel::table("floors")->add(array("id"=>$this->id,"plan" => $this->plan, "building_id" => $this->building_id))->send(); 
 if($result!=null)
 _MainModel::viewJSON($result);
 else _MainModel::viewJSON(["error" => "Данные не найдены"]);
 }
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+ }
+}
 //Удаление этажа
 public function deleteFloor(){
+if(isset($_GET['id']))
 _MainModel::table("floors")->delete(array("id" => $this->id))->send();
+
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+ }
 }
 //Редактирование этажа
 public function updateFloor(){
+if(isset($_GET['id'])&&isset($_GET['plan'])&&isset($_GET['building_id'])){
 _MainModel::table("floors")->edit(array("plan" => $this->plan, "building_id" =>$this->building_id), array("id" => $this->id))->send(); 
 }
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+}
 
+}
 }
 ?>
--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -36,19 +36,37 @@ public function getListRooms(){
 
 //Добавление 
 public function addNewRoom(){
+
+if(isset($_GET['id'])&&isset($_GET['room_name'])&&isset($_GET['floor_id'])&&isset($_GET['tag'])){
 $result=_MainModel::table("rooms")->add(array("id"=>$this->id,"room_name" => $this->room_name, "floor_id" => $this->floor_id,"tag"=>$this->tag))->send(); 
 if($result!=null)
 _MainModel::viewJSON($result);
 else _MainModel::viewJSON(["error" => "Данные не найдены"]);
 }
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+}
+}
 //Удаление 
 public function deleteRoom(){
+if(isset($_GET['id']))
 _MainModel::table("rooms")->delete(array("id" => $this->id))->send();
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+}
 }
 //Редактирование 
 public function updateRoom(){
+
+if(isset($_GET['id'])&&isset($_GET['room_name'])&&isset($_GET['floor_id'])&&isset($_GET['tag'])){
 _MainModel::table("rooms")->edit(array("room_name" => $this->room_name, "floor_id" => $this->floor_id,"tag"=>$this->tag), array("id" => $this->id))->send(); 
 }
-
+else{
+ _MainModel::viewJSON(["error" => "Неверные параметры"]);
+ die();
+}
+}
 }
 ?>
