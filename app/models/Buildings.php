f8c8208deb1de974efee5d00790515906a901dad
Bestonggg <43250876+Bestonggg@users.noreply.github.com>
2020-03-18 15:42:35 +0300

    1.8

--- app/models/Buildings.php
+++ app/models/Buildings.php
@@ -43,10 +43,7 @@ else _MainModel::viewJSON(["error" => "Данные не найдены"]);
 
 //Добавление записи
 public function addNewBuilding(){
-if(empty($_GET['town']&&$_GET['street']&&$_GET['numder']){
-_MainModel::viewJSON(["error" => "Неверные параметры"]);
-die();
-}
+
 $result=_MainModel::table("buildings")->add(array("town" => $this->town, "street" => $this->street,"number"=>$this->number))->send(); 
 
 if($result!=null)
