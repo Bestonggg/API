3fba0b05a8dcf34fa6f0ab3bf65bf146503f385b
Bestonggg <43250876+Bestonggg@users.noreply.github.com>
2020-03-19 18:20:27 +0300

    1.8

--- app/models/Buildings.php
+++ app/models/Buildings.php
@@ -41,7 +41,7 @@ public function getListBuildings(){
 $result= _MainModel::table("buildings")->get(array("id", "town","street","number"))->pagination(0,6)->send();
 if($result!=null){
  json_encode($result);
-_MainModel::viewJSON($result);
+//_MainModel::viewJSON($result);
 }
 else _MainModel::viewJSON(["error" => "Данные не найдены"]);
 }
